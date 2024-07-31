5de64afe97de919cc8827f7d7eb798892ac0b3ee
LucasG04 <mail4lg@yahoo.de>
2024-07-31 10:01:49 +0200

    removes angular content

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { NgDragDropModule } from './src/ng-drag-drop.module';
-export { NgDragDropService } from './src/services/ng-drag-drop.service';
-export { DropEvent } from './src/shared/drop-event.model';
